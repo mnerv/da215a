77d81aaf8179498f91a6c4ac79b727d97ff49f40
KTheXIII <5091397-KTheXIII@users.noreply.gitlab.com>
2020-12-09 02:23:40 +0100

    Added Makefile for Lab04
    Added delay subroutines in assembly, doesn't work

--- Lab04/src/delay/delay_asm.s
+++ Lab04/src/delay/delay_asm.s
@@ -1,8 +1,104 @@
-﻿/*
- * delay_asm.s
- *
- * Author:  ????
- *
- * Date:    ?????
- */ 
+﻿; #include <avr/io.h>
+#include <avr/io.h>
 
+.GLOBAL delay_1_micros
+.GLOBAL delay_micros
+.GLOBAL delay_ms
+.GLOBAL delay_1_s
+.GLOBAL delay_s
+
+; #include <delay.h>
+
+;---------------------------------------------------------------- 
+; delay_asm.s
+;
+; Author:  Pratchaya Khansomboon
+;
+; Date:    09-12-2020
+;---------------------------------------------------------------- 
+
+;==============================================================================
+; Delay of 1 μs (including RCALL)
+;==============================================================================
+delay_1_micros:       ; UPPGIFT: komplettera med ett antal NOP-instruktioner!!!
+  NOP                 ; 
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  RET                 ; 4 cycles
+
+;==============================================================================
+; Delay of X μs
+; LDI + RCALL = 4 cycles
+; R24 can't be more than 0xFF
+;==============================================================================
+delay_micros:         ; UPPGIFT: komplettera med ett antal NOP-instruktioner!!!
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  NOP
+  DEC R24             ; 1 cycle
+  CPI R24, 0          ; 1 cycle
+  BRNE delay_micros   ; continue! 1 false, 2 true
+  RET                 ; 4 cycles
+
+;==============================================================================
+; Delay of X ms
+; LDI + RCALL = 4 cycles
+;==============================================================================
+delay_ms:
+  MOV R18, R24
+loop_dms:
+  LDI R24, 250
+  RCALL delay_micros
+  LDI R24, 250
+  RCALL delay_micros
+  LDI R24, 250
+  RCALL delay_micros
+  LDI R24, 250
+  RCALL delay_micros
+  DEC R18
+  CPI R18, 0          ; more loops to do?
+  BRNE loop_dms       ; continue!
+  RET
+
+delay_1_s:
+  LDI R24, 250
+  RCALL delay_ms
+  LDI R24, 250
+  RCALL delay_ms
+  LDI R24, 250
+  RCALL delay_ms
+  LDI R24, 250
+  RCALL delay_ms
+  RET
+
+delay_s: 
+  MOV R19, R24
+loop_s:
+  LDI R24, 250
+  RCALL delay_ms
+  LDI R24, 250
+  RCALL delay_ms
+  LDI R24, 250
+  RCALL delay_ms
+  LDI R24, 250
+  RCALL delay_ms
+
+  DEC R19
+  CPI R19, 0        ; more loops to do?
+  BRNE loop_s       ; continue!
+  RET
--- Lab04/src/main.c
+++ Lab04/src/main.c
@@ -18,21 +18,12 @@
 
 #include <avr/io.h>
 
-#define F_CPU 16000000UL
-#include <util/delay.h>
-
-// #include "guess_nr.h"
-// #include "hmi/hmi.h"
-// #include "random/random.h"
+#include "guess_nr.h"
+#include "hmi/hmi.h"
+#include "random/random.h"
 
 int main(void) {
-    DDRC = 0x80;
-
     while (1) {
-        PORTC = 0x80;  // 0b10000000 = 128
-        _delay_ms(200);
-        PORTC = 0x00;
-        _delay_ms(250);
     }
 
     return 0;
